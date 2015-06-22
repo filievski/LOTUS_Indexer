23bf810f1c8fb6fa729ddb9b7283943e128f9f2d
filip.dbrsk <filip.dbrsk@gmail.com>
2015-06-22 13:57:54 +0200

    cleaning...

--- frank_documents_for.sh
+++ frank_documents_for.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 while read -r downloadLink; do
         echo $downloadLink
-        curl -q -o - $downloadLink | zcat | grep '"' | nodejs --max-old-space-size=8192 bulk_index.js
+        curl -q -o - $downloadLink | zcat | grep '"' | nodejs --max-old-space-size=8192 literals_in_es.js
 	#curl -q -o - $downloadLink | zcat > /dev/null        
-echo "done"
+	echo "done"
 done < <( ./Frank/frank documents --downloadUri )
