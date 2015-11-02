caea0cff7204848da6dace2438f20019753464f2
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-11-02 21:10:08 +0100

    improved incremental index script: instantiate properly, and add sanity check

--- incremental_index.sh
+++ incremental_index.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+[ -z "$1" ] && echo "Pass download link as argument" && exit 1;
 downloadLink=$1
 docid=${downloadLink##*/}
-curl -q -o - $downloadLink | zcat | grep '"' | nodejs literals_in_es.js $docid
+currentDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+curl -q -o - $downloadLink | zcat | grep '"' | nodejs $currentDir/literals_in_es.js $docid
